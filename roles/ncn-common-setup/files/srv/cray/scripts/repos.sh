70df05b158bb9f9e17d7bf7dda5d1cea2f6b1d68
Russell Bunch <doomslayer@hpe.com>
2022-06-24 08:36:29 -0500

    Shellcheck

--- roles/ncn-common-setup/files/srv/cray/scripts/repos.sh
+++ roles/ncn-common-setup/files/srv/cray/scripts/repos.sh
@@ -36,7 +36,7 @@ EOF
 }
 
 function remove-comments-and-empty-lines() {
-  sed -e 's/#.*$//' -e '/^[[:space:]]*$/d' "$@"
+  sed -e 's/#.*$//' -e '/^[[:space:]]*$/d'
 }
 
 function zypper-add-repos() {

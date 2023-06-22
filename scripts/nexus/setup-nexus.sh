dfd3a7b4cd08ccc3a8efee8fb31b0d71c963e4df
Russell Bunch <doomslayer@hpe.com>
2023-06-22 15:08:53 -0500

    MTL-2032 Fix typo (#204)

--- scripts/nexus/setup-nexus.sh
+++ scripts/nexus/setup-nexus.sh
@@ -187,6 +187,7 @@ function nexus-proxy() {
 	remove-comments-and-empty-lines "$repo_file" | \
 	while read -r url name flags; do
             url="$(echo ${url} | sed 's/'"${ARTIFACTORY_USER}"':'"${ARTIFACTORY_TOKEN}"'@//' | awk -F, '{print $1}')"
+	        name="$(echo $name | sed -e 's/${releasever_major}/'"${sle_major}"'/' -e 's/${releasever_minor}/'"${sle_minor}"'/' -e 's/${basearch}/'"${basearch}"'/' -e 's/${releasever}/'"${sle_version}"'/')"
 	        url="$(echo $url | sed -e 's/${releasever_major}/'"${sle_major}"'/' -e 's/${releasever_minor}/'"${sle_minor}"'/' -e 's/${basearch}/'"${basearch}"'/' -e 's/${releasever}/'"${sle_version}"'/')"
             echo $name $url
             curl \
@@ -488,7 +489,7 @@ function nexus-upload() {
 
 # If no overrides are set, fetch the credentials.
 if [ -z "${NEXUS_USERNAME:-}" ] && [ -z "${NEXUS_PASSWORD:-}" ]; then
-    if ! nexus-get-credentials; then
+    if ! nexus-get-credential; then
         echo >&2 'Unable to resolve NEXUS_USERNAME and NEXUS_PASSWORD from Kubernetes secret, assuming default ..'
         NEXUS_USERNAME="$DEFAULT_NEXUS_USERNAME"
         NEXUS_PASSWORD="$DEFAULT_NEXUS_PASSWORD"
